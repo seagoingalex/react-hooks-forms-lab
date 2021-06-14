8f6a59312a23cee7d4dcec45d6d694e3792a6ca5
Alex Calvert <alexanderajcalvert@gmail.com>
2021-06-14 07:50:59 -0700

    commit tree e04c0cab92fe98910a0e3075606b98bf41c62e49

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,47 @@
-import React from "react";
+import { React, useState } from "react";
 import { v4 as uuid } from "uuid";
+// The `uuid` library can be used to generate a unique id
+function ItemForm({ onItemFormSubmit }) {
 
-function ItemForm(props) {
+  // Create hooks to render a CONTROLLED form
+  const [itemName, setName] = useState("")
+  const [itemCategory, setCategory] = useState("Produce")
+
+  // Handle the form name input
+  function handleNameInput(e) {
+    setName(e.target.value)
+  }
+    
+  // Handle the form category input
+  function handleCategorySelect(e) {
+    setCategory(e.target.value)
+  }  
+
+  // Declare the callback function when a new grocery item is submitted
+  function formSubmit(e) {
+    //It's a form, so always do your preventDefault()
+    e.preventDefault();
+    // Create a new grocery based on the things that were input
+    // into the controlled form 
+    const newGrocery = { 
+      id: uuid(),
+      name: itemName, 
+      category: itemCategory }  
+    // And run the function passed in as a property to update the 
+    // grocery list array in the ShoppingList component!
+    onItemFormSubmit(newGrocery)
+  }
+
   return (
-    <form className="NewItem">
+    <form onSubmit={formSubmit} className="NewItem">
       <label>
         Name:
-        <input type="text" name="name" />
+        <input onChange={handleNameInput} type="text" name="name" value = {itemName} />
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select onChange={handleCategorySelect} value={itemCategory} name="category">
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -4,22 +4,61 @@ import Filter from "./Filter";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  // Set up hook so selecting a category dynamically filters the 
+  // grocery list
   const [selectedCategory, setSelectedCategory] = useState("All");
+  // Set up hook for filtering shopping list when searching for an item
+  const [searchQuery, setFilterByQuery] = useState("")
+  // Set up hook for the new grocery submit state
+  const [allGroceryData, setSubmittedData] = useState(items)
 
+  // Handle the addition when a new grocery has been submitted and add to
+  // the array
+  function handleSubmit(newGrocery) {
+    const newGroceryArray = [...allGroceryData, newGrocery]
+    setSubmittedData(newGroceryArray)
+  }
+  
+  // Handle the search filter state
+  function handleSearchFilter(e) {
+    setFilterByQuery(e.target.value)
+  }
+
+  // Create a binding that filters ALL THE GROCERY DATA (including new
+  // form submissions) based on the search query. .includes allows for
+  // partial case matching!
+  const itemsFilteredBySearch = allGroceryData.filter((item) => {
+    if (searchQuery === "") {
+      return item;
+    } else if (item.name.toLowerCase().includes(searchQuery.toLowerCase())) {
+      return item;
+    }
+  })
+
+  // Handles the selected category state
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
+  // Builds off of the search query filter to subsequently filter if
+  // a category is selected
+  const itemsToDisplay = itemsFilteredBySearch.filter((item) => {
     if (selectedCategory === "All") return true;
-
     return item.category === selectedCategory;
   });
 
+
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm 
+        onItemFormSubmit = {handleSubmit}
+      />
+      <Filter 
+        search={searchQuery}
+        category={selectedCategory}
+        onSearchChange={handleSearchFilter} 
+        onCategoryChange={handleCategoryChange} 
+      />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
